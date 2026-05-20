843ea5433e6ce1981ad2da2bb732bda3a9e21c1d
mosbackup <mosbackupphotos@gmail.com>
2026-05-20 16:57:13 +0200

    Update static.ts

--- server/static.ts
+++ server/static.ts
@@ -3,7 +3,10 @@ import fs from "fs";
 import path from "path";
 
 export function serveStatic(app: Express) {
-  const distPath = path.resolve(process.cwd(), "dist/public");
+  // __dirname in dist/index.cjs = the dist/ folder
+  // so public/ is right next to it
+  const distPath = path.resolve(__dirname, "public");
+
   if (!fs.existsSync(distPath)) {
     throw new Error(
       `Could not find the build directory: ${distPath}, make sure to build the client first`,
@@ -12,7 +15,6 @@ export function serveStatic(app: Express) {
 
   app.use(express.static(distPath));
 
-  // fall through to index.html if the file doesn't exist
   app.use("/{*path}", (_req, res) => {
     res.sendFile(path.resolve(distPath, "index.html"));
   });
